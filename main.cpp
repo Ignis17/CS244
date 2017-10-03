5019043f4e136eb13b76a3ba2a18969c5dd8d2f1
Ignis17 <joelturbi@yahoo.com>
2017-10-03 16:52:14 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "class.h"
+using namespace Exercises;
+
+int main()
+{
+  Exercise ex; // Exercises object
+  ex.x = 3; // x field of object ex
+  ex.y = "Hello"; // y field of object ex
+
+  ex.print(); // print() method of object ex
+  std:: cout << "\n";
+  return 0;
+}
