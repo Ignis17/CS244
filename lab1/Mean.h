dc07732cbef59d21d283b45693185419506e95e5
Ignis17 <joelturbi@yahoo.com>
2017-10-11 11:28:41 -0400

    (Lab #1)

--- lab1/Mean.h
+++ lab1/Mean.h
@@ -7,7 +7,14 @@ namespace labI
 {
 	float Mean(float data[], int size)
 	{
-		return -1;
+		float sum, mean = 0.0;          
+
+   		for (int i = 0; i < size; i++)
+   		{                           
+      			sum += data[i];                                
+   		}                                         
+   		mean = ((float)sum)/size;
+		return mean;
 	}
 }
 
--- lab1/Sort.h
+++ lab1/Sort.h
@@ -7,7 +7,21 @@ namespace labI
 {
 	void Sort(float data[],int size)
 	{
-			return;
-	}			
+		int temp;
+		for(int i = 0; i < size; i++)
+    {
+			for(int j = 0;j < size - 1; j++)
+			{
+				if(data[j] > data[j+1])
+				{
+					//we need to swap
+          temp = data[j];
+          data[j] = data[j+1];
+          data[j+1] = temp;
+        }
+      }
+    }
+		return;
+	}
 }
 #endif
