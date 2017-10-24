7f93684cb1d6e94cf49862040b5d4c0c8721a4bb
Ignis17 <joel.turbi@student.mec.cuny.edu>
2017-10-24 17:58:53 -0400

    m squares and main

--- 10_10_17/main.cpp
+++ 10_10_17/main.cpp
@@ -1,14 +1,18 @@
-#include "Squares.h"
+#include "squares.h"
 #include <iostream>
-using namespace shapes;
+using namespace example;
 
 int main()
 {
   Square1 sq1; // Invoking the default constructor of Square1
   Square2 sq2; // Invoking the default constructor f Square2
-  
+
   // Objects created using Overloaded constructors
   Square1 sq3(5);
   Square2 sq4(4);
+
+  std::cout << sq3.Perimeter() << " " << sq4.ToString();
+  std::cout << "\n";
+
   return 0;
 }
--- 10_10_17/squares.h
+++ 10_10_17/squares.h
@@ -12,42 +12,42 @@ namespace example
 		//Class Fields
 		//============
 		int side;
-		
+
 		//Class Methods
 		//=============
-		
+
 		//Default Constructor
-		/*It delagates operations to 
-		another constructor by use of 
-		the initalization list which 
-		can only be implemented by 
+		/*It delagates operations to
+		another constructor by use of
+		the initalization list which
+		can only be implemented by
 		constructors*/
 		Square1() : Square1(1) {}
-		
+
 		//Overloaded Constructor
-		Square1(int side) 
+		Square1(int side)
 		{
 			/*To resolve ambiguity
-			between the field and 
-			parameter, the this 
+			between the field and
+			parameter, the this
 			pointer is used. The this
-			pointer is a pointer that 
+			pointer is a pointer that
 			references the class object*/
 			this->side = side;
 		}
-		
+
 		//Copy Constructor
-		Square1(const Square1& other) 
+		Square1(const Square1& other)
 		{
-			/*This copy constructor 
-			performs a shallow copy (or 
+			/*This copy constructor
+			performs a shallow copy (or
 			memberwise copy) which means
-			each field is assigned the 
+			each field is assigned the
 			values by using simply using
 			the assignment operator*/
 			side = other.side;
 		}
-		
+
 		//Assignment Operator
 		Square1 operator=(const Square1& rhs)
 		{
@@ -57,58 +57,58 @@ namespace example
 			{
 				this->side = rhs.side;
 			}
-			
+
 			return *this;
 		}
-		
+
 		//Destructor
 		~Square1() {}
-		
+
 		int Perimeter()
 		{
 			return 4 * side;
 		}
-		
-		string ToString()
+
+		std::string ToString()
 		{
-			stringstream out;
+			std::stringstream out;
 			out << side;
 			return out.str();
 		}
 	};
-	
+
 	class Square2
 	{
 	 public:
 		//Class Fields
 		//============
 		int* side;
-		
+
 		//Class Methods
 		//=============
-		
+
 		//Default Constructor
-		/*It delagates operations to 
-		another constructor by use of 
-		the initalization list which 
-		can only be implemented by 
+		/*It delagates operations to
+		another constructor by use of
+		the initalization list which
+		can only be implemented by
 		constructors*/
 		Square2() : Square2(1) {}
-		
+
 		//Overloaded Constructor
-		Square2(int side) 
+		Square2(int side)
 		{
 			this->side = new int;
 			*(this->side) = side;
 		}
-		
+
 		//Copy Constructor
-		Square2(const Square2& other) 
+		Square2(const Square2& other)
 		{
-			/*This copy constructor 
-			performs a deep copy 
+			/*This copy constructor
+			performs a deep copy
 			which means it allocates new
-			memory for its fields and 
+			memory for its fields and
 			then assign them the values of
 			the other objects' fields. A
 			deep copy is only needed whenever
@@ -116,7 +116,7 @@ namespace example
 			side = new int;
 			*side = *(other.side);
 		}
-		
+
 		//Assignment Operator
 		Square2 operator=(const Square2& rhs)
 		{
@@ -124,12 +124,12 @@ namespace example
 			{
 				*(this->side) = *(rhs.side);
 			}
-			
+
 			return *this;
 		}
-		
+
 		//Destructor
-		~Square2() 
+		~Square2()
 		{
 			if(side != NULL)
 			{
@@ -137,15 +137,15 @@ namespace example
 				side = NULL;
 			}
 		}
-		
+
 		int Perimeter()
 		{
 			return 4 * (*side);
 		}
-		
-		string ToString()
+
+		std::string ToString()
 		{
-			stringstream out;
+			std::stringstream out;
 			out << *side;
 			return out.str();
 		}
