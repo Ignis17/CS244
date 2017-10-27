e86b1dc4ce8a742e5bff337d79436eaed063961f
Ignis17 <joel.turbi@student.mec.cuny.edu>
2017-10-26 22:29:29 -0400

    circles class

---
+++ circle-class/circle.h
@@ -0,0 +1,25 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+class Circle
+{
+  public:
+  int radius;
+  double PI;
+  Circle()
+  {
+    radius = 1;
+    PI = 3.14156;
+  }
+  Circle(const Circle& other)
+  {
+    radius = (other.radius);
+    PI = (other.PI);
+  }
+  double  Circumference()
+  {
+    return 2*PI*radius;
+  }
+};
+
+#endif
---
+++ circle-class/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "circle.h"
+
+
+using namespace std;
+
+
+int main()
+{
+  Circle cir1;
+  cout << cir1.Circumference() << "\n";
+  return 0;
+}
